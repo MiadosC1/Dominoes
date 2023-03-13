894e4ffe459eac2cf32cbf7e97dff2be3c2e9222
Ricardo Ibarra <116777631+MiadosC1@users.noreply.github.com>
2023-03-13 19:54:50 +0000

    saludos

--- Dominoes/Program.cs
+++ Dominoes/Program.cs
@@ -5,22 +5,24 @@
 
     public Dominoes(int domino, int domino2)
     {
-        Domino1 = domino;
-        Domino2 = domino2;
+        this.Domino1 = domino;
+        this.Domino2 = domino2;
     }
 
     public static Dominoes operator +(Dominoes a, Dominoes b)
     {
      int Superior = a.Domino1 + b.Domino1;
      int Inferior = a.Domino2 + b.Domino2;
-     int sumatotal = Superior + Inferior;
      return new Dominoes(Superior, Inferior);   
     }
-}
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Dominoes d1 = new Dominoes (0,1);
+        Dominoes d2 = new Dominoes (0,2);
+        int total = d1 + d2;
+        Console.WriteLine(total);
     }
+}
 }
